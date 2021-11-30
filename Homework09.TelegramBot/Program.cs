e1003b6628126086c1451f00cfb584fa2af98e42
eka0907@mail.ru <eka0907@mail.ru>
2021-12-01 00:33:06 +0300

    Add project files.

---
+++ Homework09.TelegramBot/Program.cs
@@ -0,0 +1,8 @@
+﻿using Telegram.Bot;
+
+var token = "";
+
+var botClient = new TelegramBotClient(token);
+
+var me = await botClient.GetMeAsync();
+Console.WriteLine($"Hello, World! I am user {me.Id} and my name is {me.FirstName}.");
